7c18178c8b3d1b25172a0a9f676b35f41770f428
yhzx233 <2058777361@qq.com>
2023-08-23 19:26:49 +0800

    init

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -4,6 +4,12 @@
  */
 
 #include <llapi/LoggerAPI.h>
+#include <llapi/EventAPI.h>
+
+#include <llapi/mc/Level.hpp>
+#include <llapi/mc/Player.hpp>
+#include <llapi/mc/MapDataManager.hpp>
+#include <llapi/mc/MapItemSavedData.hpp>
 
 #include "version.h"
 
@@ -17,5 +23,16 @@ extern Logger logger;
 void PluginInit()
 {
     // Your code here
-    logger.info("Hello, world!");
+    // logger.info("Hello, world!");
+
+    Event::PlayerLeftEvent::subscribe([](Event::PlayerLeftEvent ev) {
+        auto player = ev.mPlayer;
+        auto& manager = dAccess<MapDataManager, 1031*8>(&(player->getLevel()));
+        // logger.info("MapDataManager size: {}", manager.getAllMapData().size());
+        for (auto& [id, data] : manager.getAllMapData()) {
+            auto& v = dAccess<std::vector<std::shared_ptr<MapItemTrackedActor>>, 96>(data.get());
+            v.erase(std::remove_if(v.begin(), v.end(), [player](auto& ptr) { return dAccess<ActorUniqueID, 8>(ptr.get()) == player->getActorUniqueId(); }), v.end());
+        }
+        return true;
+	});
 }
--- src/version.h
+++ src/version.h
@@ -18,16 +18,16 @@
 // Please FILL UP the content in this area before developing your plugin, and DO NOT modify any code
 // outside this area.
 
-static_assert(false, "Please modify your plugin information below and delete this line!");
+// static_assert(false, "Please modify your plugin information below and delete this line!");
 
 // The basic descriptions of your plugin
 // Note that the the plugin name CAN ONLY CONTAIN Enlish alphabet, digit, dash(-) and
 // underscore(_). 
-#define PLUGIN_NAME "HelloWorldPrinter"
+#define PLUGIN_NAME "FixChunkLeak"
 // Describe what your plugin does
 // We recommend you start the introduction with a third-person singular verb.
-#define PLUGIN_INTRODUCTION "Prints \"Hello, World!\" in the console."
-#define PLUGIN_AUTHOR "Me"
+#define PLUGIN_INTRODUCTION "Fix LevelChunk leaking due to MapItemTrackedActor"
+#define PLUGIN_AUTHOR "yhzx233"
 
 // The version of your plugin. If you do not know how to set it, please refer
 // to https://semver.org/ .
@@ -45,7 +45,7 @@ static_assert(false, "Please modify your plugin information below and delete thi
 // please start the latest version of LiteLoaderBDS and get the protocol version.
 // If you are confident about the version compatibility of your plugin, you can
 // just comment this line (NOT RECOMMENDED).
-#define TARGET_BDS_PROTOCOL_VERSION 582
+#define TARGET_BDS_PROTOCOL_VERSION 594
 
 // ***************************************************************************************
 // ***************************************************************************************
